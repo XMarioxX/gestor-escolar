4a0098858cb0d09bdca49a116cdc2f582e98ed2f
German <germants100@gmail.com>
2024-12-21 14:57:39 -0600

    Adding first group to the side bar

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -1,21 +1,55 @@
+import { Calendar, Home, Inbox, Search, Settings } from "lucide-react"
+
 import {
     Sidebar,
     SidebarContent,
     SidebarFooter,
     SidebarGroup,
+    SidebarGroupContent,
+    SidebarGroupLabel,
     SidebarHeader,
-  } from "@/components/ui/sidebar"
-  
-  export function AppSidebar() {
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
+} from "@/components/ui/sidebar"
+
+const items = [
+    {
+        title: "Materias",
+        url: "/materias",
+        icon: Home,
+    }
+]
+
+export function AppSidebar() {
     return (
-      <Sidebar>
-        <SidebarHeader />
-        <SidebarContent>
-          <SidebarGroup />
-          <SidebarGroup />
-        </SidebarContent>
-        <SidebarFooter />
-      </Sidebar>
+        <Sidebar>
+            <SidebarHeader />
+
+            <SidebarContent>
+
+                <SidebarGroup>
+                    <SidebarGroupLabel>General</SidebarGroupLabel>
+                    <SidebarGroupContent>
+                        <SidebarMenu>
+                            {items.map((item) => (
+                                <SidebarMenuItem key={item.title}>
+                                    <SidebarMenuButton asChild>
+                                        <a href={item.url}>
+                                            <item.icon />
+                                            <span>{item.title}</span>
+                                        </a>
+                                    </SidebarMenuButton>
+                                </SidebarMenuItem>
+                            ))}
+                        </SidebarMenu>
+                    </SidebarGroupContent>
+                </SidebarGroup>
+
+
+            </SidebarContent>
+
+            <SidebarFooter />
+        </Sidebar>
     )
-  }
-  
+}
