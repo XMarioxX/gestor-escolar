6b60d748d7c44c338fa9d6114ef379082bb9e4a1
German <germants100@gmail.com>
2024-12-21 15:25:25 -0600

    Adding title to sidebar

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -1,4 +1,4 @@
-import { Calendar, Home, Inbox, Search, Settings } from "lucide-react"
+import { Calendar, Home, Inbox, Search, Settings, LibraryBig, GalleryVerticalEnd } from "lucide-react"
 
 import {
     Sidebar,
@@ -16,9 +16,14 @@ import Link from "next/link"
 
 const items = [
     {
+        title: "Inicio",
+        url: "/",
+        icon: Home,
+    },
+    {
         title: "Materias",
         url: "/Materias",
-        icon: Home,
+        icon: LibraryBig,
     }
 ]
 
@@ -27,10 +32,20 @@ export function AppSidebar() {
         <Sidebar>
             <SidebarHeader />
 
+            <SidebarMenu>
+                <SidebarMenuItem>
+                    <div className="flex flex-col gap-0.5 leading-none border border-black bg-blue-200 mx-10 justify-center items-center p-2 rounded-lg">
+                        <span className="font-semibold">Gestor Escolar</span>
+                    </div>
+                    {/*Aqui ira el toogle para los temas */}
+
+                </SidebarMenuItem>
+            </SidebarMenu>
+
             <SidebarContent>
 
                 <SidebarGroup>
-                    <SidebarGroupLabel>General</SidebarGroupLabel>
+                    {/* <SidebarGroupLabel>General</SidebarGroupLabel> */}
                     <SidebarGroupContent>
                         <SidebarMenu>
                             {items.map((item) => (
